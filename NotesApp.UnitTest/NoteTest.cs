fa09094febf12fd6ea316255f8715669a8e1132e
Thangamani.Durairaj <thangamani_d@hotmail.com>
2018-05-16 21:25:50 +0530

    NotesApp.UnitTest

---
+++ NotesApp.UnitTest/NoteTest.cs
@@ -0,0 +1,84 @@
+﻿using NotesApp.Data.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NotesApp.Controllers;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using NotesApp.Data.Infrastructure;
+using Moq;
+using System.Data.Entity;
+
+namespace NotesApp.UnitTest
+{
+    [TestClass]
+    public class NoteTest
+    {
+        [TestMethod]
+        public  void Get()
+        {
+            NotesApiController notesController = new NotesApiController();
+           
+            List<tblNote> result =  notesController.GetNotes() ;
+            
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public async Task AddNote()
+        {
+            NotesApiController notesController = new NotesApiController();
+
+            tblNote tblNote = new tblNote();
+            tblNote.Title = "New Note";
+            tblNote.Content = "Note Content";
+            tblNote.IsPin = 0;
+            tblNote.ColorCode = "black";
+            tblNote.Mode = "1";
+            tblNote.IsTrash = 1;
+            tblNote.IsActive = 0;
+           var result =await notesController.AddNote(tblNote);
+
+            Assert.AreEqual(1, result);
+
+            Assert.IsNotNull(result);
+        }
+
+
+        [TestMethod]
+        public async Task UpdateNote()
+        {
+            NotesApiController notesController = new NotesApiController();
+
+            tblNote tblNote = new tblNote();
+            tblNote.ID = 9;
+            tblNote.Title = "Update Note";
+            tblNote.Content = "Note Content";
+            tblNote.IsPin = 0;
+            tblNote.ColorCode = "white";
+            tblNote.Mode = "2";
+            tblNote.IsTrash = 0;
+            tblNote.IsActive = 0;
+
+            var result = await notesController.AddNote(tblNote);
+
+            Assert.AreEqual(1,result);
+        }
+
+        [TestMethod]
+        public async Task DeleteNote()
+        {
+            NotesApiController notesController = new NotesApiController();
+
+            tblNote tblNote = new tblNote();
+            tblNote.ID = 7;
+           
+            tblNote.Mode = "3";
+            tblNote.IsDelete = 1;
+
+            var result = await notesController.AddNote(tblNote);
+
+            Assert.AreEqual(1,result);
+        }
+    }
+}
---
+++ NotesAppDBScript.sql
@@ -0,0 +1,50 @@
+﻿
+CREATE TABLE tblRegister(
+    FirstName    VARCHAR (20) NULL,
+    LastName    VARCHAR (20) NULL,
+    Gender   VARCHAR (20) NULL,
+    BirthDate      VARCHAR (20) NULL,
+    Aadhar   VARCHAR (20) NULL,
+    MobileNumber   VARCHAR (20) NULL,
+    Address1 VARCHAR (20) NULL,
+    Address2 VARCHAR (20) NULL,
+    City     VARCHAR (20) NULL,
+    District VARCHAR (20) NULL,
+    Pin      VARCHAR (20) NULL,
+    State    VARCHAR (20) NULL,
+	Country     VARCHAR (20) NULL,
+    Email    VARCHAR (20) NULL,
+    Password VARCHAR (20) NULL
+);
+
+create proc spRegister
+(
+@FirstName varchar(20),
+@Lastname varchar(20),
+@Gender varchar(20),
+@BirthDate      VARCHAR (20),
+@Aadhar   VARCHAR(20),
+@MobileNumber   VARCHAR(20),
+@Address1 VARCHAR(20),
+@Address2 VARCHAR (20),
+@City     VARCHAR (20) ,
+@District VARCHAR (20) ,
+@Pin      VARCHAR (20) ,
+@State    VARCHAR (20) ,
+@Country     VARCHAR (20),
+@Email    VARCHAR (20), 
+@Password VARCHAR (20) 
+)
+as
+begin 
+if not exists(select * from tblRegister where Email=@Email)
+begin
+insert into tblRegister(FirstName,LastName,Gender,BirthDate,Aadhar,MobileNumber,Address1,Address2,City,District,Pin,State,Country,Email,Password) 
+values (@FirstName,@Lastname,@Gender,@BirthDate,@Aadhar,@MobileNumber,@Address1,@Address2,@City,@District,@Pin,@State,@Country,@Email,@Password)
+end
+else
+begin
+raiserror('user already exists',11,1)
+end
+end 
+go
