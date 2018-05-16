2aa7474d228a4aca3ea0bc0f6481b8dbeb9845a8
Thangamani.Durairaj <thangamani_d@hotmail.com>
2018-05-16 19:28:14 +0530

    NotesApp UI Added

---
+++ NotesApp/Controllers/DirectiveController.cs
@@ -0,0 +1,39 @@
+﻿using NotesApp.Data.Models;
+using NotesApp.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NotesApp.Controllers
+{
+    public class DirectiveController : Controller
+    {
+        // GET: Directive
+        public ActionResult Header()
+        {
+            return View();
+        }
+
+        //GET: /Directive/SideNav
+        [HttpGet]
+        public ActionResult SideNav(string UserID)
+        {
+            ////UserID= "c6dd5399-18c6-420c-abd0-d15d0a726565";
+            //var url = Request.Url.AbsoluteUri;
+
+            //if (UserID!=null)
+            //{
+            //    var list = new List<tblNote>();
+
+            //    AccountsRepository accountsRepository = new AccountsRepository();
+            //    list = accountsRepository.GetInfo(UserID);
+            //    return View(list);
+            //}
+            return View();
+        }
+
+       
+    }
+}
---
+++ NotesApp/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NotesApp.Data;
+[assembly: OwinStartup(typeof(NotesApp.Data.Startup))]
+
+namespace NotesApp
+{
+    public partial class Startup
+    {
+       
+    }
+}
